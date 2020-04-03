df1bea8853a98ff833dde67f4f03105d3e7f1a9e
EternalRat <kreygamers@gmail.com>
2020-04-03 20:50:05 +0200

    m

--- src/fr/eter/radiationzone/Check_time_and_made_rain.java
+++ src/fr/eter/radiationzone/Check_time_and_made_rain.java
@@ -9,6 +9,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 public class Check_time_and_made_rain extends BukkitRunnable {
 
 	private RadiationZone main;
+	private int demi = 0, quart = 0, last = 0, rain = 0;
 
 	public Check_time_and_made_rain(RadiationZone radiationZone) {
 		this.main = radiationZone;
@@ -19,18 +20,25 @@ public class Check_time_and_made_rain extends BukkitRunnable {
 		Bukkit.getServer().getPlayer("Rz_RazZer").sendMessage("§4" + Calendar.getInstance().get(Calendar.MINUTE) + " §aVoici le nombre de minutes");
 		for (Player player : main.Players)
 			Bukkit.getServer().getPlayer("Rz_RazZer").sendMessage("Voici les personnes dans la liste &4" + player.getName());
-		if (Calendar.getInstance().get(Calendar.MINUTE) == 30) {
+		if (Calendar.getInstance().get(Calendar.MINUTE) == 30 && demi == 0) {
+			rain = 0;
 			String toSend = "The rain will fall under 30 minutes";
 			for (Player player : main.Players)
 				player.sendTitle(toSend, "Protect yourself if you don't want to die", 10, 80, 20);
+			demi += 1;
 		}
-		if (Calendar.getInstance().get(Calendar.MINUTE) == 45)
+		if (Calendar.getInstance().get(Calendar.MINUTE) == 45 && quart == 0) {
 			for (Player player : main.Players)
 				player.sendTitle("The rain will fall under 15 minutes", "Protect yourself if you don't want to die", 10, 80, 20);
-		if (Calendar.getInstance().get(Calendar.MINUTE) == 55)
+			quart += 1;
+		}
+		if (Calendar.getInstance().get(Calendar.MINUTE) == 55 && last == 0) {
 			for (Player player : main.Players)
 				player.sendTitle("The rain will fall under 5 minutes", "Protect yourself if you don't want to die", 10, 80, 20);
-		if (Calendar.getInstance().get(Calendar.MINUTE) == 0) {
+			last += 1;
+		}
+		if (Calendar.getInstance().get(Calendar.MINUTE) == 0 && rain == 0) {
+			rain += 1;
 			Bukkit.getServer().getWorld("world").setThundering(true);
 			Bukkit.getServer().getWorld("world").setStorm(true);
 			Bukkit.getServer().getWorld("world").setWeatherDuration(6000);
